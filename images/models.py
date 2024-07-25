3a684dfb9c5e854fcc7bc36511179c1b959b9b7a
MKade19 <kadetov51@gmail.com>
2024-07-25 20:36:55 +0300

    add-image-upload

--- images/models.py
+++ images/models.py
@@ -1,4 +1,9 @@
 from django.db import models
+from datetime import datetime
+
+def upload_to(instance, filename):
+    current_time = datetime.today()
+    return 'images/{current_time}-{filename}'.format(filename=filename, current_time=current_time)
 
 class Image(models.Model):
-    image = models.ImageField(upload_to='images/')
+    image_url = models.ImageField(upload_to=upload_to, blank=True, null=True)
--- images/views.py
+++ images/views.py
@@ -1,3 +1,12 @@
-from django.shortcuts import render
+from .models import Image
+from .serializers import ImageSerializer
+from rest_framework import permissions
+from rest_framework.parsers import MultiPartParser, FormParser
+from rest_framework import viewsets
 
-# Create your views here.
+
+class ImageViewSet(viewsets.ModelViewSet):
+    queryset = Image.objects.all()
+    serializer_class = ImageSerializer
+    parser_classes = (MultiPartParser, FormParser)
+    permission_classes = [permissions.IsAuthenticated]
