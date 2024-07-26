0be26bcedf535f1f6b33bc8c5316f284cf6df057
MKade19 <kadetov51@gmail.com>
2024-07-26 19:17:23 +0300

    add-image-editing

--- images/views.py
+++ images/views.py
@@ -3,6 +3,8 @@ from .serializers import ImageSerializer
 from rest_framework import permissions
 from rest_framework.parsers import MultiPartParser, FormParser
 from rest_framework import viewsets
+import os
+from django.conf import settings
 
 
 class ImageViewSet(viewsets.ModelViewSet):
@@ -10,3 +12,12 @@ class ImageViewSet(viewsets.ModelViewSet):
     serializer_class = ImageSerializer
     parser_classes = (MultiPartParser, FormParser)
     permission_classes = [permissions.IsAuthenticated]
+
+    def destroy(self, request, *args, **kwargs):
+        instance = self.get_object()
+        data = self.get_serializer(instance).data
+        filename = data['image_url'][data['image_url'].rfind('/')+1:]
+
+        os.remove(os.path.join(settings.MEDIA_ROOT, 'images', filename))
+
+        return super().destroy(request, *args, **kwargs)
